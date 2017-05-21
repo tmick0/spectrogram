5dc632d8179c74526e01c4a4aa59c400d498589f
lq0000 <root@lo.calho.st>
2017-05-20 20:35:37 -0600

    refactor wav parsing and overlapped windowing into separate modules, optimize computation of normalization and conversion to dB by delaying until entire array is populated

--- spectrogram.py
+++ spectrogram.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 import numpy as np
-import wave
 import sys
+
+from wavwrapper import wavfile, monowrapper
+from windowing import overlapped_window
 
 # Attempt to load progressbar2 library. If it is not available, declare
 # a stub ProgressBar class that does nothing.
@@ -31,28 +33,15 @@ def main(input_file=None, window_size="1024", scale="log"):
         return 1
 
     # Open wave file and load frame rate, number of channels, sample width, and number of frames.
-    w = wave.open(input_file, 'r')
-    sample_freq = w.getframerate()
-    chans = w.getnchannels()
-    width = w.getsampwidth()
-    frame = w.getnframes()
-    
-    # Use an 8-bit integer for single-byte samples, 16-bit integer for 2-byte samples.
-    if width == 1:
-        dtype = np.uint8
-    elif width == 2:
-        dtype = np.int16
-    else:
-        sys.stderr.write("error: only 8-bit and 16-bit signed samples are supported\n")
-        return 1
+    w = wavfile(input_file)
     
     # Catch case where there are more than 2 channels.
-    if chans > 2:
+    if w.get_param('nchannels') > 2:
         sys.stderr.write("error: only mono and stereo tracks are supported\n")
         return 1
     
     # Catch case where there is less than one window of audio.
-    if frame < window_size:
+    if w.get_param('nframes') < window_size:
         sys.stderr.write("error: audio file is shorter than configured window size\n")
         return 1
     
@@ -65,75 +54,31 @@ def main(input_file=None, window_size="1024", scale="log"):
     # Y will hold the DFT of each window. We use acc and bar for displaying progress.
     Y = []
     acc = 0
-    bar = ProgressBar(max_value=frame*overlap)
-    
-    # X will hold residual window content for overlapping.
-    X = []
+    bar = ProgressBar(max_value=w.get_param('nframes') * overlap)
     
     # Process each window of audio.
-    while True:
-        
-        # If this is the first read, load an entire window, otherwise just 1/overlap of a window.
-        read_size = window_size if len(X) == 0 else window_size // overlap
-        
-        # Load raw audio data into a numpy array.
-        x = w.readframes(read_size)
-        x = np.fromstring(x, dtype=dtype)
-        
-        # If the window read was short, end.
-        if len(x) != read_size * chans:
-            break
-        
-        # If audio was 8-bit unsigned, cast it to signed and shift it down.
-        if width == 1:
-            x = (x.astype(np.int16) -  np.power(2, 7)).astype(np.int8)
-        
-        # Reshape our array into an N*c matrix in order to separate the channels.
-        x = np.reshape(x, (read_size, chans))
-
-        # Average the channels if the audio is stereo.
-        if chans > 1:
-            x = (x[:,0] + x[:,1]) / 2
-        else:
-            x = x[:,0]
-        
-        # Append these frames to the window.
-        X.extend(x)
-        
-        # Perform the FFT.
-        y = np.fft.rfft(X * hann)
-        
-        # Only data up to window_size/2 is useful; the rest is past the Nyquist cutoff.
-        y = y[:window_size//2]
-        
-        # Normalize by obtaining magnitude, multiplying by 2 (since we discarded half the FFT) and dividing by the window size.
-        y = np.absolute(y) * 2.0 / np.sum(hann)
-        
-        # Scale according to reference level, which depends on the sample width.
-        y = y / np.power(2.0, (8*width - 1))
-        
-        # Convert to dB.
-        y = (20.0 * np.log10(y)).clip(-120)
-        
-        # Add this DFT frame to the output, update the progress bar, and truncate the window.
+    for x in overlapped_window(monowrapper(w), window_size, overlap):
+        y = np.fft.rfft(x * hann)[:window_size//2]
         Y.append(y)
         acc += window_size
         bar.update(acc)
-        X = X[window_size // overlap:]
-        
+    
     # Inform progress bar that the computation is complete.
     bar.finish()
+    
+    # Normalize data and convert to dB.
+    Y = np.column_stack(Y)
+    Y = np.absolute(Y) * 2.0 / np.sum(hann)
+    Y = Y / np.power(2.0, (8 * w.get_param('sampwidth') - 1))
+    Y = (20.0 * np.log10(Y)).clip(-120)
 
-    # Time domain: We have len(Y) windows, so convert to seconds by multiplying
+    # Time domain: We have Y.shape[1] windows, so convert to seconds by multiplying
     # by window size, dividing by sample rate, and dividing by the overlap rate.
-    t = np.arange(0, len(Y), dtype=np.float) * window_size / sample_freq / overlap
+    t = np.arange(0, Y.shape[1], dtype=np.float) * window_size / w.get_param('framerate') / overlap
     
     # Frequency domain: There are window_size/2 frequencies represented, and we scale
     # by dividing by window size and multiplying by sample frequency.
-    f = np.arange(0, window_size / 2, dtype=np.float) * sample_freq / window_size
-    
-    # By default, numpy will arrange the matrix by rows, so we need to transpose it.
-    Y = np.array(Y).transpose()
+    f = np.arange(0, window_size / 2, dtype=np.float) * w.get_param('framerate') / window_size
     
     # Plot the spectrogram.
     ax = plt.subplot(111)
